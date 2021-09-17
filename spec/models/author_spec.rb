93905d3488392f96e207c10e815d08d3634b642d
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-17 17:59:58 -0500

    Eliminando el codigo autogenerado que evitaba errores en un spec vacío.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   describe "validations" do
     it "validate presence of required" do
@@ -10,5 +9,5 @@ RSpec.describe Author, type: :model do
       should validate_presence_of(:age)
     end  
   end
-  
+
 end
