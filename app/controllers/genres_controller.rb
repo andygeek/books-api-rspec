7e6f81f640cb2e88eeff75b5efb0d41a454f84a4
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-18 00:48:35 -0500

    Contruyendo las pruebas para el request GET /genres/{:id}

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,8 +1,18 @@
 class GenresController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: [], status: :not_found
+  end
+
   # GET /genres
   def index
     @genres = Genre.all
     render json: @genres, status: :ok
+  end
+
+  # GET /genres/{:id}
+  def show
+    @genre = Genre.find(params[:id])
+    render json: @genre, status: :ok
   end
 end
--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -37,4 +37,26 @@ RSpec.describe "Genres", type: :request do
     end
 
   end
+
+  describe "GET /genres/id" do
+
+    context "when nothig is found" do
+      it "should not deliver anything" do
+        get "/genres/#{rand(1..10)}"
+        payload = JSON.parse(response.body)
+        expect(payload.size).to eq(0)
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+    context "when the searched record was found" do
+      let!(:genre) { create(:genre) }
+      it "should deliver one element" do
+        get "/genres/#{genre.id}"
+        payload = JSON.parse(response.body)
+        expect(payload).to_not be_empty
+        expect(response).to have_http_status(:success)
+      end
+    end
+    
+  end
 end
